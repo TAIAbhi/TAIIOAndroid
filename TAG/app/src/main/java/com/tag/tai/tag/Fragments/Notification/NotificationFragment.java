052d2360401e8dea46ac4e07611f7b0a22e8aadb
Rishab Jaiswal <shivam.jaiswal175@gmail.com>
2018-08-09 21:43:35 +0530

    Fix:: Reformatting, notification fragment...

--- TAG/app/src/main/java/com/tag/tai/tag/Fragments/Notification/NotificationFragment.java
+++ TAG/app/src/main/java/com/tag/tai/tag/Fragments/Notification/NotificationFragment.java
@@ -41,7 +41,7 @@ import retrofit2.Response;
 
 import static android.view.View.VISIBLE;
 
-public class NotificationFragment extends Fragment implements NotificationListeners,RecyclerItemTouchHelper.RecyclerItemTouchHelperListener {
+public class NotificationFragment extends Fragment implements NotificationListeners, RecyclerItemTouchHelper.RecyclerItemTouchHelperListener {
 
     SessionManager session;
 
@@ -62,13 +62,13 @@ public class NotificationFragment extends Fragment implements NotificationListen
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View v = inflater.inflate(R.layout.fragment_notification,container,false);
+        View v = inflater.inflate(R.layout.fragment_notification, container, false);
 
         session = new SessionManager(getActivity());
 
         recyc_notification = v.findViewById(R.id.recyc_notification);
         allnotifications = new ArrayList<>();
-        notificationsAdapter = new NotificationsAdapter(allnotifications,getActivity(),this);
+        notificationsAdapter = new NotificationsAdapter(allnotifications, getActivity(), this);
 
         recyc_notification.setItemAnimator(new DefaultItemAnimator());
         recyc_notification.setLayoutManager(new LinearLayoutManager(getActivity()));
@@ -94,17 +94,17 @@ public class NotificationFragment extends Fragment implements NotificationListen
             public void onResponse(Call<NotificationResponse> call, Response<NotificationResponse> response) {
 //                Toast.makeText(getActivity(), "Received Notifications", Toast.LENGTH_SHORT).show();
 
-                if(response.code() == 200){
+                if (response.code() == 200) {
 
-                    if(response.body().getData().size() > 0){
+                    if (response.body().getData().size() > 0) {
                         allnotifications.addAll(response.body().getData());
                         notificationsAdapter.notifyDataSetChanged();
-                    }else{
+                    } else {
                         tv_notification_warning.setVisibility(VISIBLE);
                     }
 
 
-                }else{
+                } else {
                     Toast.makeText(getActivity(), "No notifications found", Toast.LENGTH_SHORT).show();
                 }
 
@@ -119,30 +119,32 @@ public class NotificationFragment extends Fragment implements NotificationListen
 
     @Override
     public void notificationDeleted(int position, com.tag.tai.tag.Services.Responses.GetNotificationResponse.Notification notification) {
-        
+
     }
 
     @Override
     public void notificationClicked(int position, com.tag.tai.tag.Services.Responses.GetNotificationResponse.Notification notification) {
+        FragmentManager fm = getActivity().getSupportFragmentManager();
 
-        if(notification.getNotificationType().equalsIgnoreCase("Default")){
-            //Toast.makeText(getActivity(), "Default", Toast.LENGTH_SHORT).show();
-        }else if(notification.getNotificationType().equalsIgnoreCase("Ranking")){
+        //default
+        if (notification.getNotificationType().equalsIgnoreCase("Default")) {
+            Toast.makeText(getActivity(), "Default", Toast.LENGTH_SHORT).show();
+        }
 
-            RankingFragment r = new RankingFragment();
-            getActivity().getSupportFragmentManager().beginTransaction().replace(R.id.container,r).commit();
+        //Ranking
+        else if (notification.getNotificationType().equalsIgnoreCase("Ranking")) {
+            fm.beginTransaction().replace(R.id.container, new RankingFragment()).commit();
+        }
 
-        }else if(notification.getNotificationType().equalsIgnoreCase("MyDetails")){
+        //my deatils
+        else if (notification.getNotificationType().equalsIgnoreCase("MyDetails")) {
+            fm.beginTransaction().replace(R.id.container, new MyDetailsFragment()).commit();
+        }
 
-            MyDetailsFragment d = new MyDetailsFragment();
-            getActivity().getSupportFragmentManager().beginTransaction().replace(R.id.container,d).commit();
-
-        }else if(notification.getNotificationType().equalsIgnoreCase("Reqdsuggprovd")){
-//            Toast.makeText(getActivity(), "requested", Toast.LENGTH_SHORT).show();
-
-            FragmentManager fm = getActivity().getSupportFragmentManager();
-
-            for(int i = 0; i < fm.getBackStackEntryCount(); i++){
+        //request for suggestion has been provided
+        else if (notification.getNotificationType().equalsIgnoreCase("Reqdsuggprovd")) {
+            Toast.makeText(getActivity(), "requestedProvided", Toast.LENGTH_SHORT).show();
+            for (int i = 0; i < fm.getBackStackEntryCount(); i++) {
                 fm.popBackStack();
             }
 
@@ -152,29 +154,28 @@ public class NotificationFragment extends Fragment implements NotificationListen
             NotificationData d = notification.getData();
             String location;
 
-            if(d.getLocationName() == null){
+            if (d.getLocationName() == null) {
                 location = "";
-            }else{
+            } else {
                 location = d.getLocationName().split("-").length > 2 ? d.getLocationName().split("-")[1] : d.getLocationName().split("-")[0];
             }
 
-
-            b.putBoolean("isFromNotification",true);
+            //setting data
+            b.putBoolean("isFromNotification", true);
             b.putString("LocationId", location);
-            b.putString("CatId",d.getCatId());
-            b.putString("SubCatId",d.getSubCatId());
-            b.putString("MCId",d.getMCId());
+            b.putString("CatId", d.getCatId());
+            b.putString("SubCatId", d.getSubCatId());
+            b.putString("MCId", d.getMCId());
 
             f.setArguments(b);
+            fm.beginTransaction().replace(R.id.container, f).commit();
+        }
 
-            fm.beginTransaction().replace(R.id.container,f).commit();
+        //request to add suggestion
+        else if (notification.getNotificationType().equalsIgnoreCase("ReqAddSug")) {
+            Toast.makeText(getActivity(), "add suggestion", Toast.LENGTH_SHORT).show();
 
-        }else if(notification.getNotificationType().equalsIgnoreCase("ReqAddSug")){
-            Toast.makeText(getActivity(), "suggadded", Toast.LENGTH_SHORT).show();
-
-            FragmentManager fm = getActivity().getSupportFragmentManager();
-
-            for(int i = 0; i < fm.getBackStackEntryCount(); i++){
+            for (int i = 0; i < fm.getBackStackEntryCount(); i++) {
                 fm.popBackStack();
             }
 
@@ -182,19 +183,18 @@ public class NotificationFragment extends Fragment implements NotificationListen
 
             Bundle b = new Bundle();
             NotificationData d = notification.getData();
-            RequestSuggestionsData rd = new RequestSuggestionsData("",d.getCategoryName(),d.getCatId(),
-                                            d.getSubCategoryName(),d.getSubCatId(),"",d.getMCId() ==  "0" ? "" : d.getMCId(),
-                                            d.getLocationName(),"","","","",
-                                            "","","","","");
+            RequestSuggestionsData rd = new RequestSuggestionsData("", d.getCategoryName(), d.getCatId(),
+                    d.getSubCategoryName(), d.getSubCatId(), "", d.getMCId() == "0" ? "" : d.getMCId(),
+                    d.getLocationName(), "", "", "", "",
+                    "", "", "", "", "");
 
-            b.putString("isARequest","true");
-            b.putParcelable("requestedSuggestion",rd);
+            b.putString("isARequest", "true");
+            b.putParcelable("requestedSuggestion", rd);
 
             f.setArguments(b);
 
-            fm.beginTransaction().replace(R.id.container,f).commit();
+            fm.beginTransaction().replace(R.id.container, f).commit();
         }
-
     }
 
     @Override
@@ -208,17 +208,16 @@ public class NotificationFragment extends Fragment implements NotificationListen
         markNotificationDismissed(notificationsAdapter.getAdapterItem(position), position);
 
 
-
     }
 
     private void markNotificationDismissed(com.tag.tai.tag.Services.Responses.GetNotificationResponse.Notification n, final int position) {
 
         Log.d(RetroClient.TAG, "markNotificationDismissed: " + n.getNotificationID());
 
-        NotificationDismissRequest request = new NotificationDismissRequest(session.getUserID(),n.getNotificationID(),"true","true");
+        NotificationDismissRequest request = new NotificationDismissRequest(session.getUserID(), n.getNotificationID(), "true", "true");
 
         Notifications notification = RetroClient.getClient().create(Notifications.class);
-        Call<UpdateNotificationData> call = notification.updateNotification(session.getToken(),request);
+        Call<UpdateNotificationData> call = notification.updateNotification(session.getToken(), request);
         call.enqueue(new Callback<UpdateNotificationData>() {
             @Override
             public void onResponse(Call<UpdateNotificationData> call, Response<UpdateNotificationData> response) {
@@ -226,7 +225,7 @@ public class NotificationFragment extends Fragment implements NotificationListen
                 Log.d(RetroClient.TAG, "onResponse: " + response.code());
                 MainActivity m = (MainActivity) getActivity();
 
-                if(m!= null){
+                if (m != null) {
                     m.checkNotificationCount();
                 }
 
@@ -236,8 +235,8 @@ public class NotificationFragment extends Fragment implements NotificationListen
 
             @Override
             public void onFailure(Call<UpdateNotificationData> call, Throwable t) {
-                if(getActivity() != null)
-                Toast.makeText(getActivity(), "Failed to dismiss the notification.", Toast.LENGTH_SHORT).show();
+                if (getActivity() != null)
+                    Toast.makeText(getActivity(), "Failed to dismiss the notification.", Toast.LENGTH_SHORT).show();
             }
         });
     }
